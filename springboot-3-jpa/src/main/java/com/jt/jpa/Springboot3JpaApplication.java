9d6db1ea950d8fe194eef00dbce8a49d719f6825
wangning <wangning@qq.com>
2021-02-06 20:18:46 +0800

    新建springboot-jpa测试

---
+++ springboot-3-jpa/src/main/java/com/jt/jpa/Springboot3JpaApplication.java
@@ -0,0 +1,13 @@
+package com.jt.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3JpaApplication.class, args);
+	}
+
+}
